340b841bcfcc6fa9931831fda42b4a7d68bd363f
MARZLOFF Martial <Martial.Marzloff@bordeaux-inp.fr>
2022-05-15 18:02:03 +0200

    bordel

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import pygame as pg
 import sys
 
 class World:
-	def __init__(self, N:int=0, Sleeping:list=[], Main:list=[], Edges:list=[], Obstacles:list=[]):
+	def __init__(self, N:int=0, Sleeping:list=[], Awake:list=[], Edges:list=[], Obstacles:list=[]):
 		self.N = N
-		self.Awake = Main
+		self.Awake = Awake
 		self.Sleeping = Sleeping
 		self.Obstacles = Obstacles
 		self.Edges = Edges
@@ -15,19 +15,18 @@ class World:
 			l = line.strip().split(" : ")
 			x = l[1].split(",")[0][1:]
 			y = l[1].split(",")[1][:-1]
-			# We suppose that there are only robots and no obstacles
+			#We suppose that there are only robots and no obstacles
 			if l[0] == 'R':
-				self.Awake.append(Robot("A", int(x), int(y)))
+				self.Awake.append(Robot(int(x), int(y)))
 			elif l[0].isnumeric():
-				self.Sleeping.append(Robot("S", int(x), int(y)))
+				self.Sleeping.append(Robot(int(x), int(y)))
 			elif l[0] == 'E':
 				self.Edges.append((
 					self.Awake[0] if x == 'R' else self.Sleeping[int(x)-1],
 					self.Awake[0] if y == 'R' else self.Sleeping[int(y)-1]
 				))
 		f.close()
-		self.N = max([self.Awake[0].x, self.Awake[0].y] +
-					 [r.x for r in self.Sleeping] + [r.y for r in self.Sleeping]) + 1
+		self.N = max([self.Awake[0].x, self.Awake[0].y] + [r.x for r in self.Sleeping] + [r.y for r in self.Sleeping]) + 1
 
 	def update(self, screen: pg.Surface, psize: int):
 		screen.fill(pg.Color("lightgray"))
@@ -35,17 +34,17 @@ class World:
 			pg.draw.circle(screen, pg.Color('red'), (r.x*psize, r.y*psize), psize//2)
 		for r in self.Sleeping:
 			pg.draw.circle(screen, pg.Color('blue'), (r.x*psize, r.y*psize), psize//2)
+	
+	def init_target(self):
+		for r in self.Awake:
+			r.targets = self.Sleeping.copy()
 			
 
 class Robot:
-	def __init__(self, type: str, x: int, y: int, targets:list=[]):
-		self.type = type
+	def __init__(self, x: int, y: int, targets:list=[]):
 		self.x = x
 		self.y = y
 		self.targets = targets
-
-	def wakeUp(self):
-		self.type = "A"
 
 
 def TowardAwakeRobot(robotA: Robot, robotS: Robot):
@@ -78,7 +77,6 @@ def closestRobot(world: World, robotA: Robot) -> Robot:
 			r = robot
 	return r
 
-
 def closestRobotInTargets(robotA: Robot, world: World) -> Robot:
 	"""Look for the closest robot"""
 	n = world.N
@@ -88,12 +86,12 @@ def closestRobotInTargets(robotA: Robot, world: World) -> Robot:
 		diff_x = abs(robot.x - robotA.x)
 		diff_y = abs(robot.y - robotA.y)
 		if (x_max + y_max > diff_x + diff_y):
-			x_max, y_max = robotA.x, robotA.y
+			x_max, y_max = diff_x, diff_y
 			r = robot
 	return r
 
 
-def compute_sub_list(robota: Robot, robotb: Robot) -> list:
+def compute_sub_list(robota: Robot, robotb: Robot, target: Robot) -> list:
 	def compute_coef(robota, robotb):
 		def compute_affine(xa, xb, ya, yb):
 			a = (yb-ya)/(xb-xa)
@@ -109,53 +107,55 @@ def compute_sub_list(robota: Robot, robotb: Robot) -> list:
 				T1.append(T[i])
 			else:
 				T2.append(T[i])
-		return [T1, T2]
-	T = robota.targets
-	if robota.x != robotb.x:
-		robota.targets = separate(T, compute_coef(robota, robotb)[
-								  0], compute_coef(robota, robotb)[1])[0]
-		robotb.targets = separate(T, compute_coef(robota, robotb)[
-								  0], compute_coef(robota, robotb)[1])[1]
-	else:
-		robota.targets = []
-		robotb.targets = []
+		return (T1,T2)
+	T=robota.targets
+	if robota.x != target.x:
+		(a, b) = compute_coef(robota, target)
+		(T1, T2) = separate(T,a,b)
+		robota.targets=T1
+		robotb.targets=T2
+	else :
+		robota.targets=[]
+		robotb.targets=[]
 		for i in range(len(T)):
 			if T[i].x > robota.x:
 				robota.targets.append(T[i])
 			else:
 				robotb.targets.append(T[i])
-	return
+	print(robotb.targets)
 
 
 def are_at_same_place(robota: Robot, robotb: Robot) -> bool:
 	return (robota.x == robotb.x and robota.y == robotb.y)
-
-
-def remove_from_sleeping(Awaken: list, Sleeping: list, robot: Robot):
-	for i in range(len(Sleeping)):
-		if Sleeping[i] == robot:
-			Awaken.append(robot)
-			Sleeping.pop(i)
-	return
 
 def wakeUp(world: World, robot: Robot):
 	robot.wakeUp()
 	world.Sleeping.remove(robot)
 	world.Awake.append(robot)
 
-def remove_from_targets(robotA: Robot, robotB: Robot):
-	for i in range(len(robotA.targets)):
-		if robotA.targets[i] == robotB:
-			robotA.targets.pop(i)
-	return
+def remove_from_targets(w,rs):
+	for r in w.Awake:
+		if len(r.targets) == 0:
+			continue
+		for r_t in r.targets:
+			if r_t == rs:
+				r.targets.remove(rs)
+				continue
+
+def awake_robot(w, idx_a, rs):
+	w.Sleeping.remove(rs)
+	w.Awake.append(rs)
+	remove_from_targets(w, rs)
+	actual = w.Awake[-1]
+	if (len (w.Awake[idx_a].targets) > 0):
+		compute_sub_list(w.Awake[idx_a], actual, closestRobotInTargets(w.Awake[idx_a], w))
 
 
 def test_execution():
 	N = 20
-	Sleeping = [Robot('S', x, y, targets) for (x, y, targets)
-				in [[0, 5, []], [5, 6, []], [12, 7, []]]]
+	Sleeping = [Robot(x, y, targets) for (x,y,targets) in [[0, 5, []], [5, 6, []], [12, 7, []]]]
 	S = len(Sleeping)
-	Main = Robot("A", N//2, N//2, Sleeping)
+	Main = Robot( N//2, N//2, Sleeping)
 	Awaken = [Main]
 	w = World(N, Awaken[0], Sleeping)
 	iterations = 0
@@ -165,17 +165,12 @@ def test_execution():
 			print(Awaken[0].targets)
 			print(closestRobot(w, Awaken[0]))
 		for i in range(len(Awaken)):
-			if len(Awaken[i].targets) != 0:
-				if are_at_same_place(Awaken[i], closestRobotInTargets(Awaken[i], w)):
-					remove_from_sleeping(
-						Awaken, Sleeping, closestRobotInTargets(Awaken[i], w))
-					closestRobotInTargets(Awaken[i], w).wakeUp
-					Awaken[len(Awaken)-1].wakeUp
-					remove_from_targets(
-						Awaken[i], closestRobotInTargets(Awaken[i], w))
-					if len(Awaken[i].targets) != 0:
-						TowardAwakeRobot(
-							Awaken[i], closestRobotInTargets(Awaken[i], w))
+			if len(Awaken[i].targets)!=0:
+				closeRT = closestRobotInTargets(Awaken[i], w)
+				if are_at_same_place(Awaken[i],closeRT) :
+					awake_robot(w, i, closeRT)
+					if len(Awaken[i].targets)!=0:
+						TowardAwakeRobot(Awaken[i], closestRobotInTargets(Awaken[i], w))
 				else:
 					TowardAwakeRobot(
 						Awaken[i], closestRobotInTargets(Awaken[i], w))
@@ -240,6 +235,36 @@ if __name__ == "__main__":
 	w = World()
 	w.init_world_from_file(sys.argv[1])
 	screen = screenInit(w, psize)
-	stupidTravellingSalesman(w, screen, psize)
+	# stupidTravellingSalesman(w, screen, psize)
+	w.init_target()
+	screen = screenInit(w, psize)
+	running = True
+	while running:
+		for event in pg.event.get():
+			if event.type == pg.QUIT:
+				running = False
+		screen.fill(pg.Color('black'))
+		iterations = 0
+		while len(w.Sleeping) > 0:
+			for i in range(len(w.Awake)):
+				screen.fill(pg.Color('black'))
+				if len(w.Awake[i].targets)!=0:
+					closeRT = closestRobotInTargets(w.Awake[i], w)
+					if are_at_same_place(w.Awake[i],closeRT) :
+						awake_robot(w, i, closeRT)
+							
+					else:
+						TowardAwakeRobot(w.Awake[i], closestRobotInTargets(w.Awake[i], w))
+				iterations+=1
+				print(iterations)
+			for a in w.Awake:
+				pg.draw.circle(screen, pg.Color('red'), (psize*a.x, psize*a.y), psize//2)
+
+			for r in w.Sleeping:
+				pg.draw.circle(screen, pg.Color('blue'), (r.x*psize, r.y*psize), psize//2)
+
+			pg.display.flip()
+			pg.time.delay(300)
+
 	pg.quit()
 	quit()
